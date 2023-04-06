2190e5c937557112b769c6c7a2ac3a51a9f42b74
jtotty <jt.totty1991@gmail.com>
2023-04-07 00:20:44 +0100

    Got TRPC running

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,1 +1,17 @@
+import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+import type { AppRouter } from '../../server/api';
 
+const client = createTRPCProxyClient<AppRouter>({
+    links: [
+        httpBatchLink({
+            url: 'http://localhost:3000/trpc',
+        }),
+    ],
+});
+
+async function main() {
+    const result = await client.logToServer.mutate('Hello from the client!');
+    console.log(result);
+}
+
+main();
--- server/api.ts
+++ server/api.ts
@@ -2,10 +2,32 @@ import express from 'express';
 import cors from 'cors';
 
 import { initTRPC } from '@trpc/server';
+import { createExpressMiddleware } from '@trpc/server/adapters/express';
 
-const trpc = initTRPC.create();
+export const t = initTRPC.create();
+const router = t.router;
+const publicProcedure = t.procedure;
+
+const appRouter = router({
+    sayHi: publicProcedure.query(() => {
+        return 'Hi!';
+    }),
+    logToServer: publicProcedure.input((v) => {
+        if (typeof v !== 'string') {
+            throw new Error('Not a string');
+        }
+        return v;
+    }).mutation((req) => {
+        console.log(`Client says: ${req.input}`);
+        return true;
+    }),
+});
 
 const app = express();
+
 app.use(cors({ origin: 'http://localhost:5173' }));
+app.use('/trpc', createExpressMiddleware({ router: appRouter }));
 
 app.listen(3000);
+
+export type AppRouter = typeof appRouter;

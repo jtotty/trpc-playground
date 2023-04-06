4524ce14cde9c217c27020217220563b89d221d7
jtotty <jt.totty1991@gmail.com>
2023-04-07 00:25:57 +0100

    Tidy up code into own files.

--- server/api.ts
+++ server/api.ts
@@ -1,27 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-
-import { initTRPC } from '@trpc/server';
 import { createExpressMiddleware } from '@trpc/server/adapters/express';
-
-export const t = initTRPC.create();
-const router = t.router;
-const publicProcedure = t.procedure;
-
-const appRouter = router({
-    sayHi: publicProcedure.query(() => {
-        return 'Hi!';
-    }),
-    logToServer: publicProcedure.input((v) => {
-        if (typeof v !== 'string') {
-            throw new Error('Not a string');
-        }
-        return v;
-    }).mutation((req) => {
-        console.log(`Client says: ${req.input}`);
-        return true;
-    }),
-});
+import { appRouter } from './routes';
 
 const app = express();
 
---
+++ server/routes/index.ts
@@ -0,0 +1,20 @@
+import { t } from '../trpc';
+
+const router = t.router;
+const publicProcedure = t.procedure;
+
+export const appRouter = router({
+    sayHi: publicProcedure.query(() => {
+        return 'Hi!';
+    }),
+    logToServer: publicProcedure.input((v) => {
+        if (typeof v !== 'string') {
+            throw new Error('Not a string');
+        }
+        return v;
+    }).mutation((req) => {
+        console.log(`Client says: ${req.input}`);
+        return true;
+    }),
+});
+
